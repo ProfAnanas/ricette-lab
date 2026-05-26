d74a14e0c79d69df841e4183f1d11bd528687333
ProfAnanas <andrea.tarditi87@gmail.com>
2026-05-26 12:12:04 +0200

    v.1.9.92
    corretto gelato fiordilatte

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = 'bobbiolab-cache-v1.9.91';
+const NOME_CACHE = 'bobbiolab-cache-v1.9.92';
 
 // Elenco dei file base da salvare per far funzionare l'interfaccia offline
 const FILE_DA_SALVARE = [
