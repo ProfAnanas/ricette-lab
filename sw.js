13d75938221a369b45b6c2b4b48c832de42ebf0f
ProfAnanas <andrea.tarditi87@gmail.com>
2026-05-26 12:15:24 +0200

    1.9.93
    bugfix ricetta

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = 'bobbiolab-cache-v1.9.92';
+const NOME_CACHE = 'bobbiolab-cache-v1.9.93';
 
 // Elenco dei file base da salvare per far funzionare l'interfaccia offline
 const FILE_DA_SALVARE = [
