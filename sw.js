3fbb22ca6bc7d0e9afb43e0a4c2056a330a59962
ProfAnanas <andrea.tarditi87@gmail.com>
2026-06-02 22:07:22 +0200

    v1.9.94
    aggiunta ricetta cremoso limone

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = 'bobbiolab-cache-v1.9.93';
+const NOME_CACHE = 'bobbiolab-cache-v1.9.94';
 
 // Elenco dei file base da salvare per far funzionare l'interfaccia offline
 const FILE_DA_SALVARE = [
